6513f62e9c6db463b43de20ad58ae2bc2d683216
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-05-12 09:22:14 +0200

    updating

--- js/jsarchived.js
+++ js/jsarchived.js
@@ -151,3 +151,112 @@ fileInput.addEventListener('change', async function () {
 
 });
 
+//no furula :(
+async function removeAds(files) {
+    for (let i = 0; i < files.length; i++) {
+        const reader = new FileReader();
+        reader.onload = async function (event) {
+
+            let fileName = files[i].name;
+            
+            const pdfData = new Uint8Array(event.target.result);
+            //const pdfDoc = await PDFLib.PDFDocument.load(pdfData, {ignoreEncryption: true },{ parseContentStream: false } );
+            const pdfDoc = await PDFLib.PDFDocument.load(pdfData, {ignoreEncryption: true });
+            //const pdfDoc = await PDFLib.PDFDocument.load(pdfData);
+            
+            const form = pdfDoc.getForm();
+            const fields = form.getFields()
+            fields.forEach(field => {
+                const type = field.constructor.name
+                const name = field.getName()
+                console.log(`${type}: ${name}`)
+            })
+            // const helveticaFont = await pdfDoc.embedFont(PDFLib.StandardFonts.Helvetica)
+            // const pages = pdfDoc.getPages();
+            // const firstPage = pages[0];
+            // const { width, height } = firstPage.getSize()
+            // firstPage.drawText('This text was added with JavaScript!', {
+            //     x: 5,
+            //     y: height / 2 + 300,
+            //     size: 50,
+            //     font: helveticaFont,
+            //     color: PDFLib.rgb(0.95, 0.1, 0.1),
+            //     rotate: PDFLib.degrees(-45),
+            // });
+
+            const pdfBytes = await pdfDoc.save();
+            const blob = new Blob([pdfBytes], { type: "application/pdf" });
+            const url = URL.createObjectURL(blob);
+            
+            const extension = fileName.split(".").pop();
+            const name = fileName.split(".").shift();
+            const newFileName = name + "_modificado." + extension;
+
+            // chrome.downloads.download({
+            //     url: url,
+            //     filename: newFileName,
+            //     saveAs: false
+            // });
+
+            //await downloadFile(newFileName, url);
+        };
+        reader.readAsArrayBuffer(files[i]);
+    }
+    }
+
+
+//si funciona boton funcional etc añade texto
+
+async function editFiles(files) {
+    for (let i = 0; i < files.length; i++) {
+        const reader = new FileReader();
+        reader.onload = async function (event) {
+
+            let fileName = files[i].name;
+            const pdfData = new Uint8Array(event.target.result);
+
+            const pdfDoc = await PDFLib.PDFDocument.load(pdfData);
+            //const pdfDoc = await PDFLib.PDFDocument.load(pdfData, {ignoreEncryption: true });
+
+            const helveticaFont = await pdfDoc.embedFont(PDFLib.StandardFonts.Helvetica)
+            const pages = pdfDoc.getPages();
+            const firstPage = pages[0];
+            const { width, height } = firstPage.getSize()
+            firstPage.drawText('This text was added with JavaScript!', {
+                x: 5,
+                y: height / 2 + 300,
+                size: 50,
+                font: helveticaFont,
+                color: PDFLib.rgb(0.95, 0.1, 0.1),
+                rotate: PDFLib.degrees(-45),
+            });                     
+            const pdfBytes = await pdfDoc.save();
+            const blob = new Blob([pdfBytes], { type: "application/pdf" });
+            const url = URL.createObjectURL(blob);
+            
+            const extension = fileName.split(".").pop();
+            const name = fileName.split(".").shift();
+            const newFileName = name + "_modificado." + extension;
+
+            // chrome.downloads.download({
+            //     url: url,
+            //     filename: newFileName,
+            //     saveAs: false
+            // });
+
+            await downloadFile(newFileName, url);
+        };
+        reader.readAsArrayBuffer(files[i]);
+    }
+  }
+
+ 
+  
+  async function downloadFile(d_fileName, d_url) {
+    chrome.downloads.download({
+        url: d_url,
+        filename: d_fileName,
+        saveAs: false
+    });
+  }
+  
