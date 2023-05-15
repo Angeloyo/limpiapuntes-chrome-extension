f9c903e8c21838589fb136d24f48ab2d2b4934a4
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-05-15 20:49:48 +0200

    pretty popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,15 @@
-//import { degrees, PDFDocument, rgb, StandardFonts } from './pdf-lib.js';
-
 let fileInput = document.getElementById("file-input");
 let fileList = document.getElementById("files-list");
 let numOfFiles = document.getElementById("num-of-files");
 let saveButton = document.getElementById("save-button");
+let loading = document.getElementById("loading");
 let saveButtonDiv = document.getElementById("save-button-div");
 
 saveButton.style.visibility = 'hidden';
+loading.style.visibility = 'hidden';
 
 fileInput.addEventListener('change', async function () {
+
     const files = this.files;
     numOfFiles.textContent = `${fileInput.files.length} Archivos seleccionados`;
     for (let i = 0; i < files.length; i++) {
@@ -28,67 +29,41 @@ fileInput.addEventListener('change', async function () {
         };
         reader.readAsArrayBuffer(files[i]);
     }
-
     saveButton.style.visibility = 'visible';
-
     saveButton.addEventListener('click', async function () {
         await removeAds(files);
     });
-
 });
 
 async function removeAds(files) {
+    loading.style.visibility = 'visible';
     for (let i = 0; i < files.length; i++) {
       const reader = new FileReader();
       reader.onload = async function (event) {
         // Send file and receive the modified one
         const formData = new FormData();
         formData.append("file", files[i]);
-  
-        const response = await fetch("https://limitless-falls-79195.herokuapp.com/upload", {
+        // http://localhost:5000/upload
+        const response = await fetch("https://fierce-fortress-98391.herokuapp.com/upload", {
           method: "POST",
           body: formData,
         });
-  
         if (response.ok) {
           const blob = await response.blob();
-  
           const url = URL.createObjectURL(blob);
-  
           let fileName = files[i].name;
           const extension = fileName.split(".").pop();
           const name = fileName.split(".").shift();
           const newFileName = name + "_limpio." + extension;
-  
           await downloadFile(newFileName, url);
+          loading.style.visibility = 'hidden';
         } else {
           console.error("Error uploading file:", response.statusText);
         }
       };
       reader.readAsArrayBuffer(files[i]);
     }
-  }
-
-//  async function removeAds(files) {
-//     for (let i = 0; i < files.length; i++) {
-//         const reader = new FileReader();
-//         reader.onload = async function (event) {
-
-//             // Send file and receive the modified one
-
-//             const url = URL.createObjectURL(blob);
-
-//             let fileName = files[i].name;
-//             const extension = fileName.split('.').pop();
-//             const name = fileName.split('.').shift();
-//             const newFileName = name + '_mod.' + extension;
-
-//             await downloadFile(newFileName, url);
-//         };
-//         reader.readAsArrayBuffer(files[i]);
-//     }
-// }
-
+}
 
 async function downloadFile(d_fileName, d_url) {
   chrome.downloads.download({
