71cffa2617e797c1d16940dabf61448b9aae280f
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-05-17 20:08:50 +0200

    minor change

--- js/popup.js
+++ js/popup.js
@@ -4,18 +4,14 @@ let numOfFiles = document.getElementById("num-of-files");
 let saveButton = document.getElementById("save-button");
 let loading = document.getElementById("loading");
 let saveButtonDiv = document.getElementById("save-button-div");
-
 saveButton.style.visibility = 'hidden';
 loading.style.visibility = 'hidden';
-
 fileInput.addEventListener('change', async function () {
-
     const files = this.files;
     numOfFiles.textContent = `${fileInput.files.length} Archivos seleccionados`;
     for (let i = 0; i < files.length; i++) {
         const reader = new FileReader();
         reader.onload = async function (event) {
-
             let listItem = document.createElement("li");
             let fileName = files[i].name;
             let fileSize = (files[i].size / 1024).toFixed(1);
@@ -25,7 +21,6 @@ fileInput.addEventListener('change', async function () {
             listItem.innerHTML = `<p>${fileName}</p><p>${fileSize}MB</p>`;
             }
             fileList.appendChild(listItem);
-
         };
         reader.readAsArrayBuffer(files[i]);
     }
@@ -34,17 +29,14 @@ fileInput.addEventListener('change', async function () {
         await removeAds(files);
     });
 });
-
 async function removeAds(files) {
   let cont = 0;
     loading.style.visibility = 'visible';
     for (let i = 0; i < files.length; i++) {
       const reader = new FileReader();
       reader.onload = async function (event) {
-        // Send file and receive the modified one
         const formData = new FormData();
         formData.append("file", files[i]);
-        // http://localhost:5000/upload
         const response = await fetch("https://enigmatic-brushlands-61693.herokuapp.com/upload", {
           method: "POST",
           body: formData,
@@ -61,17 +53,14 @@ async function removeAds(files) {
         } else {
           console.error("Error uploading file:", response.statusText);
         }
-
         cont++;
         if(cont==files.length){
           loading.style.visibility = 'hidden';
         }
-
       };
       reader.readAsArrayBuffer(files[i]);
     }
 }
-
 async function downloadFile(d_fileName, d_url) {
   chrome.downloads.download({
       url: d_url,
