fe000c585077d1f468b1047ad6a3bcd8d979128d
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-06-10 01:35:58 +0200

    v1.1.3.0

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,28 @@
 let fileInput = document.getElementById("file-input");
 let fileList = document.getElementById("files-list");
-let eList = document.getElementById("e-list");
 let numOfFiles = document.getElementById("num-of-files");
 let saveButton = document.getElementById("save-button");
 let loading = document.getElementById("loading");
 let saveButtonDiv = document.getElementById("save-button-div");
 let errormsg = document.getElementById("id-errormsg");
 let errormsg2 = document.getElementById("id-errormsg2");
+let errormsg3 = document.getElementById("id-errormsg3");
 let divzip = document.getElementById("div_zip");
 let cbx = document.getElementById("cbx");
 let msg = document.getElementById("id-msg");
 let infomsg = document.getElementById("info-msg");
+let pacienciamsg = document.getElementById("paciencia-msg");
+
 saveButton.style.visibility = 'hidden';
 loading.style.visibility = 'hidden';
+pacienciamsg.style.visibility = 'hidden';
+
 let numFiles = 0;
 const zip = new JSZip();
 window.addEventListener('load', loadmsg);
 function loadmsg() {
   const now = Date.now();
-  fetch(`https://limpiapuntes.com/ext/msgv2.txt?${now}`)
+  fetch(`https://limpiapuntes.com/ext/msg-v1.1.3.txt?${now}`)
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -75,196 +79,59 @@ fileInput.addEventListener('change', async function () {
     });
 });
 
-function cola_paginas(num_paginas){
-  let cola = [];
-  if (num_paginas <= 10) {
-    cola.push('P'); // 1
-    cola.push('B'); // 2
-    if (num_paginas > 2) {
-      cola.push('A'); // 3
-      if (num_paginas > 3) {
-        if (num_paginas === 4) {
-          cola.push('A') // 4
-        } else {
-          cola.push('P'); // 4
-          cola.push('A'); // 5
-          cola.push('B'); // 6
-          if (num_paginas > 6) { // no existen de 5
-            cola.push('A'); // 7
-            if (num_paginas > 7) {
-              cola.push('A'); // 8
-              if (num_paginas > 8) {
-                cola.push('B'); // 9
-                if (num_paginas > 9) { // es 10
-                  cola.push('A'); // 10
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  } else { // 11 pages or more
-    if (num_paginas >= 101){ //101 pages or more
-      cola.push('P'); // 1
-      for(var i = 0 ; i < num_paginas - 1 ; i++){
-        cola.push('A')
-      }
-    }
-    else{ //between 11 and 101
-      cola.push('P'); // 1
-      cola.push('B'); // 2
-      cola.push('A'); // 3
-      cola.push('P'); // 4
-      cola.push('A'); // 5
-      cola.push('A'); // 6
-      cola.push('B'); // 7
-      cola.push('A'); // 8
-      cola.push('A'); // 9
-      cola.push('A'); // 10
-      cola.push('B'); // 11
-      let cont = 11;
-      let restan = num_paginas - cont;
-      while (restan >= 4) {
-        cola.push('A');
-        cola.push('A');
-        cola.push('A');
-        cola.push('B');
-        restan = restan - 4;
-      }
-      if (restan === 1) {
-        cola.push('A');
-      } else if (restan === 2) {
-        cola.push('A');
-        cola.push('A');
-      } else if (restan === 3) {
-        cola.push('A');
-        cola.push('A');
-        cola.push('A');
-      }
-    }
-  }
-  return cola;
-}
 async function removeAds(files) {
   let checked = false;
   if (numFiles > 1) {
     checked = cbx.checked;
   }
   loading.style.visibility = 'visible';
+  pacienciamsg.style.visibility = 'visible';
   const processFile = async (file) => {
-    try{
-      if (!verificarPDF(file)) {
-        errormsg.textContent = 'Alguno de los archivos seleccionados no tiene extensión .pdf';
-        numFiles--;
-        return;
-      }
-      infomsg.textContent = "Procesando " + file.name;
-      const reader = new FileReader();
-      const fileLoaded = new Promise((resolve) => {
-        reader.onload = resolve;
+    if (!verificarPDF(file)) {
+      errormsg.textContent = 'Alguno de los archivos seleccionados no tiene extensión .pdf';
+      numFiles--;
+      return;
+    }
+    const reader = new FileReader();
+    const fileLoaded = new Promise((resolve) => {
+      reader.onload = resolve;
+    });
+    reader.readAsArrayBuffer(file);
+    await fileLoaded;
+    infomsg.textContent = "Procesando " + file.name + " ...";
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      //https://shrouded-shelf-55195.herokuapp.com/upload
+      // https://infinite-journey-17186.herokuapp.com/upload
+      //http://localhost:5000/upload
+      const response = await fetch("https://infinite-journey-17186.herokuapp.com/upload", {
+        method: "POST",
+        body: formData,
       });
-      reader.readAsArrayBuffer(file);
-      await fileLoaded;
-      const pdfData = new Uint8Array(reader.result);
-      const pdfDoc = await PDFLib.PDFDocument.load(pdfData);
-      const pages = pdfDoc.getPages();
-      const numpaginas_original = pages.length;
-      let cola = cola_paginas(numpaginas_original);
-      for (const page of pages) {
-        let nueva_accion = cola.shift();
-        if(nueva_accion != 'P'){
-          const xObjects = page.node
-          .Resources()
-          .lookup(PDFLib.PDFName.of('XObject'), PDFLib.PDFDict);
-          const targetWidth = 390;
-          const targetHeight = 71;
-          let targetKey = null;
-          const xObjectsArray = Array.from(xObjects.dict);
-          xObjectsArray.forEach(([key, value]) => {
-            if (value instanceof PDFLib.PDFRef) {
-              const xObject = pdfDoc.context.lookup(value);
-              if (xObject instanceof PDFLib.PDFStream) {
-                const subtype = xObject.dict.lookup(PDFLib.PDFName.of('Subtype'));
-                if (subtype === PDFLib.PDFName.of('Image')) {
-                  const width = xObject.dict.lookup(PDFLib.PDFName.of('Width'));
-                  const height = xObject.dict.lookup(PDFLib.PDFName.of('Height'));
-                  console.log("wid: " + width);
-                  console.log("height: " + height);
-                  if (width == targetWidth && height == targetHeight) {
-                    targetKey = key;
-                    const imageRef = xObjects.get(targetKey);
-                    pdfDoc.context.delete(imageRef);
-                  }
-                }
-              }
-            }
-          });
-          const { width, height } = page.getSize();
-          if(nueva_accion == 'B'){
-            const scaleFactor = 1.117;
-            page.scale(scaleFactor, scaleFactor);
-            const n_x = (width*scaleFactor) * 0.144;
-            const n_y = (height*scaleFactor) * 0.027 ;
-            const n_height = ((height*scaleFactor) * 0.86308) - ((height*scaleFactor) * 0.027);
-            const n_width = ((width*scaleFactor) * 0.982) - ((width*scaleFactor) * 0.144);
-            page.setMediaBox(n_x, n_y, n_width, n_height);
-            page.setBleedBox(n_x, n_y, n_width, n_height);
-            page.setArtBox(n_x, n_y, n_width, n_height);
-            page.setTrimBox(n_x, n_y, n_width, n_height);
-            page.setCropBox(n_x, n_y, n_width, n_height);
-
-          }
-          else if(nueva_accion == 'A'){
-            const n_x = width * 0.02;
-            const n_y = height * 0.05;
-            const n_height = (height * 0.985) - (height * 0.05);
-            const n_width = (width * 0.955) - (width * 0.02);
-            page.setMediaBox(n_x, n_y, n_width, n_height);
-            page.setBleedBox(n_x, n_y, n_width, n_height);
-            page.setArtBox(n_x, n_y, n_width, n_height);
-            page.setTrimBox(n_x, n_y, n_width, n_height);
-            page.setCropBox(n_x, n_y, n_width, n_height);
-          }
+      if (response.ok) {
+        const blob = await response.blob();
+        let fileName = file.name;
+        const extension = fileName.split(".").pop();
+        const name = fileName.split(".").shift();
+        const newFileName = name + "_limpiapuntes." + extension;
+        if (numFiles > 1 && checked) {
+          zip.file(newFileName, blob);
+        } else {
+          const url = URL.createObjectURL(blob);
+          await downloadFile(newFileName, url);
+        }
+      } else {
+        const contentType = response.headers.get("Content-Type");
+        if (contentType.includes("application/json")) {
+          const responseData = await response.json();
+          errormsg3.textContent = responseData.Error;
         }
       }
-      pdfDoc.removePage(0);
-      if(numpaginas_original > 4){
-        pdfDoc.removePage(2);
-      }
-      if(numpaginas_original == 101){
-        errormsg2.textContent = 'Has adjuntado un archivo que tiene exactamente 101 páginas, y hay un error conocido para archivos con este numero de paginas en concreto, por lo que es posible que no se haya procesado correctamente. Disculpa las molestias.';
-      }
-      for (const page of pdfDoc.getPages()) {
-        const annotationsRef = page.node.Annots();
-        if (annotationsRef === undefined) continue;
-        const annotations = pdfDoc.context.lookup(annotationsRef);
-        const annotationsArray = annotations.asArray();
-        annotationsArray.forEach((annotationRef) => {
-            const annotation = pdfDoc.context.lookup(annotationRef);
-            if (annotation && annotation.get(PDFLib.PDFName.of('Subtype')) === PDFLib.PDFName.of('Link')) {
-              pdfDoc.context.delete(annotationRef);
-            }
-        });
-      }
-      const pdfBytes = await pdfDoc.save();
-      const elblob = new Blob([pdfBytes], { type: "application/pdf" });
-      let fileName = file.name;
-      const extension = fileName.split(".").pop();
-      const name = fileName.split(".").shift();
-      const newFileName = name + "_limpiapuntes." + extension;
-      if (numFiles > 1 && checked) {
-        zip.file(newFileName, elblob);
-      } else {
-        const url = URL.createObjectURL(elblob);
-        await downloadFile(newFileName, url);
-      }
-  } catch (error) {
-    console.log(error);
-    let listItem = document.createElement("li");
-    listItem.innerHTML = `<p class="errormsg"> No podemos procesar el archivo: ${file.name} </p>`;
-    eList.appendChild(listItem);
-  }
+    } catch (error) {
+      errormsg2.textContent = "Parece que no tienes conexión a internet o el servidor no funciona correctamente.";
+      console.log(error);
+    }
   };
   const filesArray = Array.from(files);
   for (const file of filesArray) {
@@ -278,7 +145,9 @@ async function removeAds(files) {
   }
   loading.style.visibility = 'hidden';
   infomsg.textContent = ""
+  pacienciamsg.style.visibility = 'hidden';
 }
+
 async function downloadFile(d_fileName, d_url) {
   chrome.downloads.download({
       url: d_url,
